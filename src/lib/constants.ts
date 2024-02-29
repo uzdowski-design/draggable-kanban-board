d659e9c8d79599a1945bae4868aa76cff5d8bee0
Michal Uzdowski <uzdowski.design@gmail.com>
2024-02-29 18:02:16 +0100

    updated mock tasks

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,28 +1,37 @@
 export const DEFAULT_CARDS = [
   // BACKLOG
-  { title: 'Look into render bug in dashboard', id: '1', column: 'backlog' },
-  { title: 'SOX compliance checklist', id: '2', column: 'backlog' },
-  { title: '[SPIKE] Migrate to Azure', id: '3', column: 'backlog' },
-  { title: 'Document Notifications service', id: '4', column: 'backlog' },
+  {
+    title: 'Investigate framer-motion event type bug',
+    id: '1',
+    column: 'backlog'
+  },
+  {
+    title: 'Research on collaboration possibilities',
+    id: '2',
+    column: 'backlog'
+  },
+  { title: 'Add database connection (NoSQL)', id: '3', column: 'backlog' },
+  { title: 'Add Authentication restriction', id: '4', column: 'backlog' },
   // TODO
   {
-    title: 'Research DB options for new microservice',
+    title: 'Implement adding, removing and editing categories',
     id: '5',
     column: 'todo'
   },
-  { title: 'Postmortem for outage', id: '6', column: 'todo' },
-  { title: 'Sync with product on Q3 roadmap', id: '7', column: 'todo' },
+  { title: 'Add priorities for tasks', id: '6', column: 'todo' },
 
   // DOING
-  {
-    title: 'Refactor context providers to use Zustand',
-    id: '8',
-    column: 'doing'
-  },
-  { title: 'Add logging to daily CRON', id: '9', column: 'doing' },
+  { title: 'Upgrade board styling', id: '7', column: 'doing' },
+
   // DONE
   {
-    title: 'Set up DD dashboards for Lambda listener',
+    title: 'Add draggable funcionality',
+    id: '8',
+    column: 'done'
+  },
+  { title: 'Add basic interface', id: '9', column: 'done' },
+  {
+    title: 'Create kanban skeleton',
     id: '10',
     column: 'done'
   }
